ad445e7f0ee36774881ca2fb70be72dbf3755da4
AidaContreras <93019827+AidaContreras@users.noreply.github.com>
2022-01-28 23:10:26 -0500

    about

---
+++ src/components/Prueba.vue
@@ -0,0 +1,37 @@
+<template>
+   <div id="Prueba">
+
+    <h1>{{ msg }}</h1>
+
+    <input 
+      type="text"
+      v-model="firstName"
+      @input="$emit('update:firstName', $event.target.value)">
+
+    <input
+      type="text"
+      v-model="lastName"
+      @input="$emit('update:lastName', $event.target.value)">
+
+   </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'Prueba',
+    props: {
+        msg: String
+    },
+    data() {
+      return {
+        firstName: 'John',
+        lastName: 'Doe',
+      };
+    }
+}
+</script>
+
+
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Prueba msg="Acerca de"></Prueba>
+
+      <p>First name: {{ firstName }}</p>
+      <p>Last name: {{ lastName }}</p>
+
+      <Prueba
+        first-name="firstName"
+        last-name="lastName"
+      ></Prueba>
+    
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Prueba from '@/components/Prueba.vue'
+
+export default {
+  name: 'About',
+    components: {
+      Prueba
+    }, 
+    data() {
+      return {
+        firstName: '',
+        lastName: ''
+      }
+    }
+}
+
+</script>
+
